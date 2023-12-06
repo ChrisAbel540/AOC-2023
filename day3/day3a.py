97b2a0382714810879cf9d7d8002c2fd6097d922
Chris Abel <cja540@gmail.com>
2023-12-06 19:16:19 +0000

    day3a solution

--- day3/day3a.py
+++ day3/day3a.py
@@ -1,42 +1,47 @@
 import numpy as np
 
+total = 0
+numbers = []
 def is_character(char):
     if (char.isalnum() or char == '.'):
         return False
     else:
         return True
 
-def return_bool_ring(data,i,j):
-    for scani in range(i-1,i+2):
-        for scanj in range(j-1,j+2):
-                data[scani][scanj] = True
+def find_number(data, i, j):
+     keepGoing = True
+     number = []
+     while keepGoing:
+          if j == len(data[i]) or not data[i][j].isdigit():
+               return number
+          else:
+               number.append(data[i][j])
+               j += 1
+
+def is_symbol_near(data, i, j, numDigits): # bug in here
+    for scanj in range(j - 1, j + numDigits + 1):
+        for scani in range(i - 1, i + 2):
+                if (scani >= 0 and scani < len(data) and scanj >= 0 and scanj < len(data[i])):
+                     if is_character(data[scani][scanj]):
+                          return True
+    return False
+
+def wipe_number(data, i, j, numDigits):
+    for x in range(j,j+numDigits):
+        data[i][x] = '.'
     return data
 
 data = []
-with open(r".\day3\Test.txt", "r") as file:
+with open(r".\day3\input.txt", "r") as file:
     for line in file:
         data.append(list(line.rstrip()))
 
-symbolBool = []
 for i in range(len(data)):
-    a = map(lambda x: is_character(x),data[i])
-    symbolBool.append(list(a))
-
-symbolHits = symbolBool
-for i in range(len(symbolHits)):
-    for j in range(len(symbolHits[0])):
-        if symbolBool[i][j] == True:
-            symbolHits = return_bool_ring(symbolHits,i,j)
-
-print(symbolHits)
-
-# def return_number(pos_i, pos_j):
-#     contLoop = True
-#     number = data[pos_i][pos_j]
-#     iteration = 1
-#     while contLoop:
-#         if not data[pos_i][pos_j + iteration].isdigit():
-#             return number
-#         else:
-#             number += data[pos_i][pos_j + iteration]
-#             iteration += 1
+     for j in range(len(data[0])):
+          if data[i][j].isdigit():
+                number = find_number(data, i, j)
+                data = wipe_number(data, i, j, len(number))
+                if is_symbol_near(data, i ,j , len(number)): # bug here  
+                    total += int(''.join(number))
+                    numbers.append(int(''.join(number)))
+print(total)
